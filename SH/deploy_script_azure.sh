4452121037ff4a8b93342dd8e81f228e7c981c87
Anderson <andersonmaliszewski@gmail.com>
2020-02-10 12:45:00 -0300

    update

--- SH/deploy_script_azure.sh
+++ SH/deploy_script_azure.sh
@@ -18,20 +18,25 @@
 #done
 
 #Create 8 virtual machines from A10 template.
-#for i in `seq 12 17`; do
+for i in `seq 1 8`; do
 az vm create \
     --admin-username iscc \
     --resource-group ISCC-2020 \
     --generate-ssh-keys \
-    --name trylast2 \
+    --name ISCC$i \
     --size Standard_A10 \
     --location westus \
     --image UbuntuLTS \
     --verbose
+
 az vm run-command invoke \
 	-g ISCC-2020 \
-	-n trylast2 \
+	-n ISCC \
 	--command-id RunShellScript \
-	--scripts "sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt install make gfortran openmpi-bin libopenmpi-dev -y"
-#done
-
+	--scripts "sudo apt update -y && 
+	sudo apt upgrade -y && 
+	sudo apt autoremove -y && 
+	sudo apt install make gfortran openmpi-bin libopenmpi-dev hwloc -y &&
+	cd $HOME; git clone --recursive --progress https://github.com/andermm/ISCC-2020.git; 
+	chown -R iscc:iscc ISCC-2020; mv ISCC-2020 /home/iscc"
+done
