8ca512d8cbe0a2ea2f2f23fd4900a6c262b850f9
Anderson <andersonmaliszewski@gmail.com>
2020-02-10 14:04:01 -0300

    update

--- SH/deploy_script_azure.sh
+++ SH/deploy_script_azure.sh
@@ -17,13 +17,39 @@
 #    --generate-ssh-keys
 #done
 
-#Create 8 virtual machines from A10 template.
-for i in `seq 1 8`; do
+#Create the master virtual machine from A10 template.
 az vm create \
     --admin-username iscc \
     --resource-group ISCC-2020 \
     --generate-ssh-keys \
-    --name A10_ISCC$i \
+    --name A10_ISCC1 \
+    --size Standard_A10 \
+    --location westus \
+    --image UbuntuLTS \
+    --verbose
+
+az vm run-command invoke \
+	-g ISCC-2020 \
+	-n A10ISCC1 \
+	--command-id RunShellScript \
+	--scripts "sudo apt update -y && 
+	sudo apt upgrade -y && 
+	sudo apt autoremove -y && 
+	sudo apt install make gfortran openmpi-bin libopenmpi-dev hwloc nfs-kernel-server -y &&
+	cd $HOME; git clone --recursive --progress https://github.com/andermm/ISCC-2020.git; 
+	chown -R iscc:iscc ISCC-2020; mv ISCC-2020 /home/iscc;
+	echo '/home/iscc/ISCC-2020 (rw,sync,no_root_squash,no_subtree_check)' >> /etc/exports;
+	exportfs -a"
+
+
+
+#Create the slaves virtual machines from A10 template.
+for i in `seq 2 8`; do
+az vm create \
+    --admin-username iscc \
+    --resource-group ISCC-2020 \
+    --generate-ssh-keys \
+    --name A10ISCC$i \
     --size Standard_A10 \
     --location westus \
     --image UbuntuLTS \
@@ -36,7 +62,7 @@ az vm run-command invoke \
 	--scripts "sudo apt update -y && 
 	sudo apt upgrade -y && 
 	sudo apt autoremove -y && 
-	sudo apt install make gfortran openmpi-bin libopenmpi-dev hwloc -y &&
-	cd $HOME; git clone --recursive --progress https://github.com/andermm/ISCC-2020.git; 
-	chown -R iscc:iscc ISCC-2020; mv ISCC-2020 /home/iscc"
+	sudo apt install make gfortran openmpi-bin libopenmpi-dev hwloc nfs-common -y &&
+	sudo mount -t nfs A10ISCC1:/home/iscc/ISCC-2020 /home/iscc;
+	echo 'A10ISCC1:/home/iscc/ISCC-2020 /home/iscc' >> /etc/fstab"
 done
--- SH/experiments_exec.sh
+++ SH/experiments_exec.sh
@@ -35,16 +35,16 @@ APP_TEST_INTEL=PingPong
 
 #Other Variables
 START=`date +"%d-%m-%Y.%Hh%Mm%Ss"`
-if [[ $(eval hostaname) == A10_ISCC1 ]]; then
+if [[ $(eval hostaname) == A10ISCC1 ]]; then
 	OUTPUT_APPS_EXEC=$LOGS/exec_A10.$START.csv
 	OUTPUT_INTEL_EXEC=$LOGS/intel_A10.$START.csv
 	CONTROL_FILE_OUTPUT=$BASE/LOGS/env_info_A10.org
-	PARTITION=(A10_ISCC1 A10_ISCC2 A10_ISCC3 A10_ISCC4 A10_ISCC5 A10_ISCC6 A10_ISCC7 A10_ISCC8)
+	PARTITION=(A10ISCC1 A10ISCC2 A10ISCC3 A10ISCC4 A10ISCC5 A10ISCC6 A10ISCC7 A10ISCC8)
 else
 	OUTPUT_APPS_EXEC=$LOGS/exec_A8.$START.csv
 	OUTPUT_INTEL_EXEC=$LOGS/intel_A8.$START.csv
 	CONTROL_FILE_OUTPUT=$BASE/LOGS/env_info_A8.org
-	PARTITION=(A8_ISCC1 A8_ISCC2 A8_ISCC3 A8_ISCC4 A8_ISCC5 A8_ISCC6 A8_ISCC7 A8_ISCC8)
+	PARTITION=(A8ISCC1 A8ISCC2 A8ISCC3 A8ISCC4 A8ISCC5 A8ISCC6 A8ISCC7 A8ISCC8)
 fi
 
 #############################################################################################################
@@ -161,7 +161,7 @@ do
 	runline+="mpiexec --mca btl self,"
 	
 #Select interface
-	if [[ $(eval hostaname) == A10_ISCC1 ]]; then
+	if [[ $(eval hostaname) == A10ISCC1 ]]; then
 		runline+="tcp --mca btl_tcp_if_include eth0 "
 	else
 		runline+="openib --mca btl_openib_if_include mlx5_0:1 "	
